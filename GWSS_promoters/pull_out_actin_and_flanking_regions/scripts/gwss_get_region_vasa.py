98c81054da147ecf274dddf1c719a3dccb3c4585
Cassie <clettinger@ucdavis.edu>
2021-01-28 08:09:37 -0800

    Fixed scaffold # in vasa file

--- GWSS_promoters/pull_out_actin_and_flanking_regions/scripts/gwss_get_region_vasa.py
+++ GWSS_promoters/pull_out_actin_and_flanking_regions/scripts/gwss_get_region_vasa.py
@@ -2,5 +2,5 @@ from get_region import *
 
 #run our code 
 print(">vasa_scaffold_602_plus10kbpregion")
-get_region("data/scaffold_10516.fasta", 326337, 334651, 5000) 
+get_region("data/scaffold_602.fasta", 326337, 334651, 5000) 
 
